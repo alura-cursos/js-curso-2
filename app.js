8c1ba3123266ab1db83b7ab98777c58e6ee2fe61
tatazin <tales3miranda@gmail.com>
2025-09-27 20:58:34 -0300

    feat: Adicionar a funcao para alterar o texto do HTML

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Digite um número entre 1 e 10';
+function textoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+textoNaTela('h1', 'Bem vindo ao jogo do número secreto ');
+textoNaTela('p', 'Digite um número entre 1 e 10');
 
 function verificarChute() {
     console.log('O botão foi clicado');
