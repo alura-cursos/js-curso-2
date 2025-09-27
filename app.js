7ba143704f5024c2ee631e5c67a5f8a6eca55717
tatazin <tales3miranda@gmail.com>
2025-09-27 19:37:19 -0300

    feature: Primeiro passos do projeto do curso 2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Vamos começar nosso programa de soma!!!';
+titulo.innerHTML = 'Jogo do número secreto';
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Digite um número entre 1 e 10';
 
 function verificarChute() {
-    let numero1 = parseInt(prompt('Digite o primeiro número:')); 
-    let numero2 = parseInt(prompt('Digite o segundo número:'));
-    alert(`O resultado dos números ${numero1} e ${numero2} é ${numero1 + numero2}`);
-}
+    console.log('O botão foi clicado');
+}    
