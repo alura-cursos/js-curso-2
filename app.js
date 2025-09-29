50dc51c1f48c9df0a750401fc0b9824fcde41304
tatazin <tales3miranda@gmail.com>
2025-09-28 21:25:40 -0300

    feat: Funcao que faz a media de 3 números

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-function dobra() {
-    return entrada * 2;
+function mostrarTexto(tag, texto) {
+    campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
-function verificarChute(){
-    entrada = parseInt(document.querySelector('input').value);
-    console.log(entrada);
-    console.log(dobra());
+mostrarTexto('h1', 'Calculadora de média!');
+mostrarTexto('p', 'Coloque aqui 3 números para calcular a média:');
+
+let soma = 0;
+let contador = 0;
+
+function calcularMedia(soma, contador) {
+    return (soma / contador);
+}
+function verificarChute() {
+    let valor = parseInt(document.querySelector('input').value);
+    soma = soma + valor;
+    document.querySelector('input').value = '';
+    contador++;
+
+    if (contador == 3) {
+        alert(calcularMedia(soma, contador));
+        contador = 0;
+        soma = 0;
+    }
 }
