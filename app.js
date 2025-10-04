668c03940c203decc240347101917e6995ccef57
tatazin <tales3miranda@gmail.com>
2025-10-04 19:30:33 -0300

    feat: App para calcular IMC

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-function quadrado(numero) {
-    return (numero*numero)
+let contador = 0;
+let altura = 0;
+let peso = 0;
+function mostrarTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
+function caucularIMC(peso,altura) {
+    return peso / (altura * altura);
+}
+function zerar() {
+    document.querySelector('input').value = '';
+}
+function verificarChute() {
+    if (contador == 0) {
+        altura = parseFloat(document.querySelector('input').value);
+        contador++;
+        zerar();
+        mostrarTexto('p', 'Agora digite aqui seu peso:');
+    } else {
+        peso = parseFloat(document.querySelector('input').value);
+        zerar();
+        mostrarTexto('p', `Seu IMC atual é de ${caucularIMC(peso,altura)} `)
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }
+}
+function reiniciar() {
+    bemVindo();
+    contador = 0;
+    peso = 0;
+    altura = 0;
+    document.getElementById('reiniciar').setAttribute('disabled','true');
+}
+function bemVindo() {
+    mostrarTexto('h1', 'Bem vindo a Calculadora de IMC!');
+    mostrarTexto('p', 'Digite aqui sua altura:');
+}
+bemVindo();
