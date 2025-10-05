f4c4c652b902442473d85de3d07a7cb3404689bb
tatazin <tales3miranda@gmail.com>
2025-10-05 19:33:27 -0300

    feat: Conclusão do projeto principal do curso

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
+let qntdTentativa = 1;
+let numeroMáximo = 10;
+let numerosJaUsados = [];
+let numeroSorteado = getRandom();
 function textoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-
-textoNaTela('h1', 'Bem vindo ao jogo do número secreto ');
-textoNaTela('p', 'Digite um número entre 1 e 10');
-
 function verificarChute() {
-    console.log(getRandom());
-}    
-
-function getRandom() {
-    return parseInt(Math.random() * 10 + 1);
+    let chute = parseInt(document.querySelector('input').value)
+    if (numeroSorteado == chute) {
+        let tentativa = (qntdTentativa == 1) ? 'tentativa' : 'tentativas';
+        textoNaTela('h1', 'Você ganhou!');
+        textoNaTela('p', `Parabéns, você acertou com ${qntdTentativa} ${tentativa}!`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('chutar').setAttribute('disabled', true);
+    } else {
+        if (chute < numeroSorteado) {
+            maiorMenor = 'Maior';
+        } else {
+            maiorMenor = 'Menor';
+        }
+        errou();
+    }
+    qntdTentativa++;
+    zerar();
 }
+function reiniciar() {
+    qntdTentativa = 1;
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    document.getElementById('chutar').removeAttribute('disabled');
+    iniciar();
+    numeroSorteado = getRandom();
+}
+function getRandom() {
+    let numeroEscolhido = parseInt(Math.random() * numeroMáximo + 1);
+    if (numerosJaUsados.length == numeroMáximo) {
+        numerosJaUsados = [];
+    }
+    if (numerosJaUsados.includes(numeroEscolhido)) {
+        return getRandom();
+    } else {
+        numerosJaUsados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+function zerar() {
+    document.querySelector('input').value = '';
+}
+function iniciar() {
+    textoNaTela('h1', 'Bem vindo ao jogo do número secreto ');
+    textoNaTela('p', `Digite um número entre 1 e ${numeroMáximo}`);
+}
+function errou() {
+    textoNaTela('h1', 'Você errou.');
+    textoNaTela('p', `O número sorteado é ${maiorMenor}`);  
+}
+iniciar();
